27b9bcf6145a76c4998bd1e485b85214c243dbd8
Макс Апаков <maksapakov@gmail.com>
2022-08-01 15:42:41 +0300

    ver 2.0

--- src/PrintTextPerRole.java
+++ src/PrintTextPerRole.java
@@ -2,10 +2,11 @@ public class PrintTextPerRole {
 public static String printTextPerRole(String[] roles, String[] textLines) {
     StringBuilder textPerRole = new StringBuilder();
     for (String role : roles) {
+        textPerRole.append("\n");
         textPerRole.append(role).append(":\n");
         for (int j = 0; j < textLines.length; j++) {
             if (textLines[j].startsWith(role.concat(":"))) {
-                textPerRole.append(textLines[j].replace(role + ": ", j + 1 + ") "));
+                textPerRole.append(textLines[j].replace(role + ": ", j + 1 + ") ").replace("\n", "") + "\n");
             }
         }
     }
